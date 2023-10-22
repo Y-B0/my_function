267062e85e5b4fc5b631da667cc56732f6e38865
Y-B0 <52906974+Y-B0@users.noreply.github.com>
2023-10-22 23:20:55 +0800

    Update ssgsea_deom.R

--- ssgsea_deom.R
+++ ssgsea_deom.R
@@ -1,14 +1,13 @@
-ssgsea_deom<-function(exp,pathway=NULL,species="Homo sapiens"){
+ssgsea_deom<-function(exp,pathway=NULL,species="Homo sapiens",category="C2",subcategory="KEGG"){
   library(msigdbr)
   library(GSVA)
   ### exp is a expression data with gene symbol as rowname and sample name as colname
   ### pathway is the gene sets list, if pathway is NULL it will default as kegg for human
-  ### species are name in msigdbr
-  if (pathway==NULL) {
-    pathway <- msigdbr(species = species, category = "C2", subcategory = "KEGG")
-    pathway <- pathway %>% split(x = .$gene_symbol, f = .$gs_name)
-    names(pathway) <- gsub("KEGG_","",names(pathway))%>%gsub("_"," ",.)
-  }
+  ### species, category and subcategory are name in msigdbr (msigdbr)
+  
+  pathway <- msigdbr(species = species, category = "C2", subcategory = "KEGG")
+  pathway <- pathway %>% split(x = .$gene_symbol, f = .$gs_name)
+  names(pathway) <- gsub("KEGG_","",names(pathway))%>%gsub("_"," ",.)
 
   gs.exp <- gsva(as.matrix(exprSet),pathway, method = "gsva")
 
