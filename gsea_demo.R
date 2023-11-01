2bcaa8540ad860ae2387c9e08789377b54136afb
Y-B0 <52906974+Y-B0@users.noreply.github.com>
2023-11-01 11:02:21 +0800

    Update gsea_demo.R

--- gsea_demo.R
+++ gsea_demo.R
@@ -1,5 +1,10 @@
 ##gsea
-gsea<-function(deg,fc.name="logFC",go.ont="BP",n=10){
+gsea<-function(symbol,rank,go.ont="ALL",GO=TRUE,species.go="org.Hs.eg.db",KEGG=TRUE,species.kegg="hsa",n=10){
+  ### gene symbol vector for the enrichment, must matched with rank
+  ### rank vector for the symbol, only matched with symbol, can be logfc or some sort index for symbol, no need to sort
+  ### go.not can be "ALL", "BP", "MF" and "CC"
+  ###
+  
   library(ggplot2)
   library(stringr)
   library(clusterProfiler)
@@ -7,38 +12,41 @@ gsea<-function(deg,fc.name="logFC",go.ont="BP",n=10){
   library(Hmisc)
   library(enrichplot)
   library(RColorBrewer)
-  gene=bitr(rownames(deg),fromType="SYMBOL",toType="ENTREZID",OrgDb="org.Hs.eg.db") 
-  gene <- dplyr::distinct(gene,SYMBOL,.keep_all=TRUE)
-  deg<-deg[match(intersect(rownames(deg),gene$SYMBOL),rownames(deg)),]
-  gene<-gene[match(intersect(rownames(deg),gene$SYMBOL),gene$SYMBOL),]
-  deg$ENTREZID<-gene$ENTREZID
-  deg<-deg[order(deg$logFC,decreasing = T),]
-  geneList = deg[,fc.name]
+
+  gene <- bitr(symbol,fromType="SYMBOL",toType="ENTREZID",OrgDb="org.Hs.eg.db")
+  deg <- data.frame(gene=gene,rank=rank)
+  deg <- dplyr::distinct(deg,gene,.keep_all=TRUE)
+  deg <- deg[order(deg$rank,decreasing = T),]
+  geneList = deg$rank
   names(geneList) <- deg$ENTREZID
   
-  gse.GO <- try({
-    gseGO(
-      geneList, 
-      ont = go.ont,  
-      OrgDb = org.Hs.eg.db,
-      keyType = "ENTREZID",
-      pvalueCutoff = 1,
-      pAdjustMethod = "BH"
-    )
-  })
-  p_go<-try({
-    gseaplot2(gse.GO,geneSetID = 1:n,pvalue_table = F,base_size = 13,color = brewer.pal(10,'Paired'))
-  })
-  #write.csv(gse.GO[[1]],"gse_GO.csv",row.names = F)
-  gse.KEGG <- try({
-    gseKEGG(geneList, 
-            organism = "hsa", 
-            pvalueCutoff = 1,
-            pAdjustMethod = "BH")
-  })
-  p_kegg<-try({gseaplot2(gse.KEGG,geneSetID = 1:n,pvalue_table = F,base_size = 13,color = brewer.pal(10,'Paired'))})
+    if (GO==T) {
+    gse.GO <- try({
+      gseGO(
+        geneList, 
+        ont = go.ont,  
+        OrgDb = species,
+        keyType = "ENTREZID",
+        pvalueCutoff = 1,
+        pAdjustMethod = "BH"
+      )
+    })
+    p_go<-try({
+      gseaplot2(gse.GO,geneSetID = 1:n,pvalue_table = F,base_size = 13,color = brewer.pal(10,'Paired'))
+    })
+  }
+
+  if (KEGG==T) {
+    gse.KEGG <- try({
+      gseKEGG(geneList, 
+              organism = "hsa", 
+              pvalueCutoff = 1,
+              pAdjustMethod = "BH")
+    })
+    p_kegg<-try({gseaplot2(gse.KEGG,geneSetID = 1:n,pvalue_table = F,base_size = 13,color = brewer.pal(10,'Paired'))})
+  }
+
   try(return(list(gse.GO=gse.GO,gse.KEGG=gse.KEGG,p_go=p_go,p_kegg=p_kegg)))
-  #write.csv(gse.KEGG[[1]],"gse_KEGG.csv",row.names = F)
 }
 
 
