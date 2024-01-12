04a12302bfee54425f38a8b9191438f388372f16
Y-B0 <52906974+Y-B0@users.noreply.github.com>
2024-01-12 16:24:09 +0800

    Add files via upload

---
+++ axis_fold.R
@@ -0,0 +1,36 @@
+
+
+squash_axis <- function(from, to, factor) { 
+  library(scales)
+  # Args:
+  #   from: left end of the axis
+  #   to: right end of the axis
+  #   factor: the compression factor of the range [from, to]
+  
+  trans <- function(x) {    
+    # get indices for the relevant regions
+    isq <- x > from & x < to
+    ito <- x >= to
+    
+    # apply transformation
+    x[isq] <- from + (x[isq] - from)/factor
+    x[ito] <- from + (to - from)/factor + (x[ito] - to)
+    
+    return(x)
+  }
+  
+  inv <- function(x) {
+    # get indices for the relevant regions
+    isq <- x > from & x < from + (to - from)/factor
+    ito <- x >= from + (to - from)/factor
+    
+    # apply transformation
+    x[isq] <- from + (x[isq] - from) * factor
+    x[ito] <- to + (x[ito] - (from + (to - from)/factor))
+    
+    return(x)
+  }
+  
+  # return the transformation
+  return(trans_new("squash_axis", trans, inv))
+}
