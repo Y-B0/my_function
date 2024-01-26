13b55b16b4f4a86722d8f1039ca4292d5f47550f
Y-B0 <52906974+Y-B0@users.noreply.github.com>
2024-01-27 00:30:43 +0800

    Update limma_demo.R

--- limma_demo.R
+++ limma_demo.R
@@ -1,36 +1,37 @@
-limma_after_normalized <- function(exp,group,compare,normalize=F,log2=F,merge=F,rna.count=F,symbol.name="SYMBOL"){
+limma_after_normalized<-function(exp,group,compared,normalize=F,log2=F,merge=F,rna.count=F,symbol.name="SYMBOL"){
   ## exp is expression data (if merge==T,exp need a col to contain gene symbol)
   ## group can be multi group ,and first col is sample name; second col is condition; coef, and multi used to select multi group
-  ## compare used to identify which group vs which group (usually <conventional> - <control>). also use for multi group condition.
-  
+  ## compared used to identify which group vs which group (usually <conventional> - <control>). also use for multi group condition.
+  print(compared)
   if (merge==T) {
-    expr = aggregate(expr, by=list(expr[,symbol.name]),mean)
-    expr = expr[!is.na(expr[,symbol.name]) & expr[,symbol.name]!="",]
-    row.names(expr) = expr[,symbol.name]
-    expr<-expr[,-grep(symbol.name,colnames(expr))]
+    exp = aggregate(exp, by=list(expr[,symbol.name]),mean)
+    exp = expr[!is.na(exp[,symbol.name]) & exp[,symbol.name]!="",]
+    row.names(expr) = exp[,symbol.name]
+    exp<-exp[,-grep(symbol.name,colnames(exp))]
   }
   
   if (rna.count==T) {
-    expr<- voom(exprSet,design,normalize="quantile")$E
+    exp<- voom(exp,design,normalize="quantile")$E
   }
   
   if (normalize==T) {
-    expr<-normalizeBetweenArrays(expr)
+    exp<-normalizeBetweenArrays(exp)
   }
   if (log2==T) {
-    expr<-log2(expr+1)
+    exp<-log2(exp+1)
   }
   
-  design <- model.matrix(~0+factor(group[,2]))
-  colnames(design) <- unique(group[,2])
-  rownames(design) <- group[,1]
-  exp<-exp[,rownames(design)]
-
-  fit <- lmFit(exp,design)
-  contrast.matrix <- makeContrasts(compare,levels = design)
+  design <- model.matrix(~0 + factor(group[, 2]))
+  colnames(design) <- unique(group[, 2])
+  rownames(design) <- group[, 1]
+  exp <- exp[, rownames(design)]
+  
+  fit <- lmFit(exp, design)
+  compared<-"W24 - baseline"
+  contrast.matrix <- makeContrasts(contrasts=compared, levels = colnames(design))  # 修改这里
   
   fit2 <- contrasts.fit(fit, contrast.matrix)
   fit2 <- eBayes(fit2)
-  output <- topTable(fit2,coef = compare,n=Inf)
+  output <- topTable(fit2, coef = compared, n = Inf)
   return(output)
 }
