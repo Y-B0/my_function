f5a69486d9fcdab089d44079755a74226f41f603
Y-B0 <52906974+Y-B0@users.noreply.github.com>
2024-01-31 22:01:12 +0800

    Update gsea_demo.R

--- gsea_demo.R
+++ gsea_demo.R
@@ -1,5 +1,4 @@
-##gsea
-gsea<-function(symbol,rank,go.ont="ALL",GO=TRUE,species.go="org.Hs.eg.db",KEGG=TRUE,species.kegg="hsa",n=10){
+function(symbol,rank,go.ont="ALL",GO=TRUE,species.go="org.Hs.eg.db",KEGG=TRUE,species.kegg="hsa",n=10){
   ### gene symbol vector for the enrichment, must matched with rank
   ### rank vector for the symbol, only matched with symbol, can be logfc or some sort index for symbol, no need to sort
   ### go.not can be "ALL", "BP", "MF" and "CC"
@@ -12,21 +11,17 @@ gsea<-function(symbol,rank,go.ont="ALL",GO=TRUE,species.go="org.Hs.eg.db",KEGG=T
   library(Hmisc)
   library(enrichplot)
   library(RColorBrewer)
-
-  gene <- bitr(symbol,fromType="SYMBOL",toType="ENTREZID",OrgDb="org.Hs.eg.db")
-  deg <- data.frame(gene=gene,rank=rank)
-  deg <- dplyr::distinct(deg,gene,.keep_all=TRUE)
-  deg <- deg[order(deg$rank,decreasing = T),]
-  geneList = deg$rank
-  names(geneList) <- deg$ENTREZID
   
-    if (GO==T) {
+  names(rank)<-symbol
+  geneList<-order(rank)
+  
+  if (GO==T) {
     gse.GO <- try({
       gseGO(
         geneList, 
         ont = go.ont,  
-        OrgDb = species,
-        keyType = "ENTREZID",
+        OrgDb = species.go,
+        keyType = "SYMBOL",
         pvalueCutoff = 1,
         pAdjustMethod = "BH"
       )
@@ -35,19 +30,16 @@ gsea<-function(symbol,rank,go.ont="ALL",GO=TRUE,species.go="org.Hs.eg.db",KEGG=T
       gseaplot2(gse.GO,geneSetID = 1:n,pvalue_table = F,base_size = 13,color = brewer.pal(10,'Paired'))
     })
   }
-
+  
   if (KEGG==T) {
     gse.KEGG <- try({
       gseKEGG(geneList, 
-              organism = "hsa", 
+              organism = species.kegg, 
               pvalueCutoff = 1,
               pAdjustMethod = "BH")
     })
     p_kegg<-try({gseaplot2(gse.KEGG,geneSetID = 1:n,pvalue_table = F,base_size = 13,color = brewer.pal(10,'Paired'))})
   }
-
+  
   try(return(list(gse.GO=gse.GO,gse.KEGG=gse.KEGG,p_go=p_go,p_kegg=p_kegg)))
 }
-
-
- 
